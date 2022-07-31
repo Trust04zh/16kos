864168840c391e70b7b16ee257f516ebcec42433
zly7 <264793917@qq.com>
2022-07-31 10:45:09 +0800

    prepare

--- kern/schedule/sched.c
+++ kern/schedule/sched.c
@@ -52,8 +52,8 @@ sched_init(void) {
     // sched_class = &default_sched_class;
     // sched_class = &stride_sched_class;
     // sched_class = &priority_sched_class;
-    // sched_class = &cfs_sched_class;
-    sched_class = &rr_p_sched_class;
+    sched_class = &cfs_sched_class;
+    // sched_class = &rr_p_sched_class;
 
     rq = &__rq;
     rq->max_time_slice = MAX_TIME_SLICE;
